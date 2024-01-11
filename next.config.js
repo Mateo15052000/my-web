345d5f626e7dd0abdf778c57d4a65975540438e7
Erick Mateo <erick15mateorosas@gmail.com>
2024-01-11 17:53:56 -0500

    pa

--- next.config.js
+++ next.config.js
@@ -53,6 +53,9 @@ const securityHeaders = [
 ]
 
 module.exports = withBundleAnalyzer({
+  images: {
+    unoptimized: true
+  }, 
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   eslint: {
